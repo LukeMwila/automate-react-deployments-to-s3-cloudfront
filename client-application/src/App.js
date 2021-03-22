e2033014dd3bcf04454433b80c1886bea6c38ae2
Luke Mwila <outlier.developer@gmail.com>
2021-03-22 13:39:25 +0200

    refactor: updated environment name

--- client-application/src/App.js
+++ client-application/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Environment: <code>Production</code>
+          Environment: <code>UAT</code>
         </p>
         <a
           className="App-link"
